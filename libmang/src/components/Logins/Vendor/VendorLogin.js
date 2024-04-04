519827c364eac9ddbef34c042c16dafc8c8568bc
Sweta Kumari <swetakumari8587@gmail.com>
2024-04-04 05:41:20 +0530

    slight changes and nodemon added

--- libmang/src/components/Logins/Vendor/VendorLogin.js
+++ libmang/src/components/Logins/Vendor/VendorLogin.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const VendorLogin = () => {
     const navigate = useNavigate();
-
+    const [showAlert, setShowAlert] = useState(false);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const handleUsernameChange = (e) => {
@@ -16,10 +16,9 @@ const VendorLogin = () => {
     }
 
     async function collectData() {
-        navigate("/LogedinVendorPortal");
         try {
-            let response = await fetch("/loginAsVendor", {
-                method: "PUT",
+            let response = await fetch("http://localhost:5000/logAsVendor", {
+                method: "POST",
                 body: JSON.stringify({
                     UserId: username,
                     Password: password,
@@ -30,8 +29,17 @@ const VendorLogin = () => {
             });
 
             if (!response.ok) {
-                console.log("Response was not ok");
+                if (response.status === 404) {
+                    setShowAlert(true); // Set showAlert state to true to show the alert
+                } else {
+                    console.log("Response was not ok");
+                }
+
             }
+            else {
+                navigate("/LogedinVendorPortal");
+            }
+
         } catch (error) {
             console.log("Not able to fetch", error);
         }
@@ -75,22 +83,27 @@ const VendorLogin = () => {
                 <div className={alstyles.user}>
                     <label className={alstyles.blabel}>Cancel</label>
 
-                    <button 
-                    className={alstyles.blabel}
-                    onClick={collectData}
+                    <button
+                        className={alstyles.blabel}
+                        onClick={collectData}
                     >
                         Login
                     </button>
+                    {/* Alert component to show when the user does not exist */}
+                    {showAlert && (
+                        <div className="alert">
+                            <p>User does not exist. Please check your credentials.</p>
+                            <button onClick={() => setShowAlert(false)}>Close</button>
+                        </div>
+                    )}
 
                 </div>
-                {/* <div>
-                    Not Vendor? <NavLink to="/SignUpAsVendor">
-                    <button className={alstyles.blabel}>Sign Up</button>
-                    </NavLink>
-                </div> */}
                 <div>
-                    New Vendor? <button className={alstyles.blabel}>SignUp</button>
+                    Not Vendor? <NavLink to="/SignUpAsVendor">
+                        <button className={alstyles.blabel}>Sign Up</button>
+                    </NavLink>
                 </div>
+
             </div>
         </>
     )
--- libmang/src/components/Logins/Vendor/VendorSignUp.js
+++ libmang/src/components/Logins/Vendor/VendorSignUp.js
@@ -1,30 +1,95 @@
 import React from "react";
+import { useState } from "react";
 import alstyles from "../Admin/AdminLogin.module.css"
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const VenderSignUp = () => {
+
+    const navigate = useNavigate();
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    }
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    async function collectData() {
+        try {
+            let response = await fetch("http://localhost:5000/SignUpAsVendor", {
+                method: "POST",
+                body: JSON.stringify({
+                    UserId: username,
+                    Password: password,
+                }),
+                headers: {
+                    "content-type": "Application/json",
+                },
+            });
+
+            if (!response.ok) {
+                console.log("Response was not ok");
+            }
+            else {
+                navigate("/logAsVendor");
+            }
+
+        } catch (error) {
+            console.log("Not able to fetch", error);
+        }
+    }
+
+
 
 
 
     return (
         <>
             <div className={alstyles.inparea}>
-            <h1><label className={alstyles.llabel}>Welcome Vendor</label></h1>
-               
-                
-                
                 <div className={alstyles.user}>
-                    
-                    <button className={alstyles.blabel}>Your Item</button>
-                    <button className={alstyles.blabel}>Add New Item</button>
-                    <button className={alstyles.blabel}>Transaction</button>
-                    <button className={alstyles.blabel}>LogOut</button>
-                    
-                    
-                    
+
+                    <label className={alstyles.llabel}>CHART</label>
+                    <NavLink to="/">
+                        <label className={alstyles.llabel}>BACK</label>
+                    </NavLink>
+
                 </div>
-                
-                
+                <h1><label className={alstyles.llabel}>Event Management System</label></h1>
+                <div className={alstyles.user}>
+                    <label className={alstyles.llabel} htmlFor="username">User Id</label>
+                    <input
+                        id="username"
+                        className={alstyles.inpbox}
+                        value={username}
+                        onChange={handleUsernameChange}
+
+                    ></input>
+                </div>
+                <div className={alstyles.user}>
+                    <label className={alstyles.llabel} htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        className={alstyles.inpbox}
+                        type="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    ></input>
+                </div>
+                <div className={alstyles.user}>
+                    <label className={alstyles.blabel}>Cancel</label>
+
+                    <button
+                        className={alstyles.blabel}
+                        onClick={collectData}
+                    >
+                        Sign Up
+                    </button>
+
+                </div>
+
             </div>
 
         </>
